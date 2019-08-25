939d2a4d1fddedd22ca65e0716a95ea2684bec18
Frederic Auberson <frederic@auberson.net>
2019-08-25 23:00:37 +0200

    Pass staging directory

--- src/main/java/riot/riotctl/Util.java
+++ src/main/java/riot/riotctl/Util.java
@@ -1,5 +1,6 @@
 package riot.riotctl;
 
+import java.io.File;
 import java.util.List;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -7,7 +8,8 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class Util {
 
-	public Util(List<Target> targets) {
+	public Util(File source, List<Target> targets) {
+		System.err.println(ToStringBuilder.reflectionToString(source, ToStringStyle.MULTI_LINE_STYLE));
 		System.err.println(ToStringBuilder.reflectionToString(targets, ToStringStyle.MULTI_LINE_STYLE));
 	}
 
