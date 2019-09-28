8c99d39952d1b9ac13771fdabf1ca5bd08043a88
Frederic Auberson <frederic@auberson.net>
2019-09-28 14:10:17 +0200

    Remember to use -Dresume=false (this script isn't executed, it's more of a notebook)

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export GPG_TTY=$(tty)
-mvn release:prepare 
+mvn release:prepare -Dresume=false 
 echo "review results in Sonatype's staging repository, then execute 'mvn release:perform'"
